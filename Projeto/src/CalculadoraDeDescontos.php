7bad814509ea160281cd139c4e25ff51761ac5ee
Renata Deodório <renatadeod@gmail.com>
2023-05-09 20:28:45 -0300

    Criando calculadora de descontos

---
+++ Projeto/src/CalculadoraDeDescontos.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHP\DesignPattern;
+
+class CalculadoraDeDescontos
+{
+  public function calculaDescontos(Orcamento $orcamento): float
+  {
+    if ($orcamento->quantidadeItens > 5) {
+      return $orcamento->valor * 0.1;
+    }
+
+    return 0;
+  }
+
+}
--- Projeto/testes.php
+++ Projeto/testes.php
@@ -1,15 +1,23 @@
 <?php
 
+use PHP\DesignPattern\CalculadoraDeDescontos;
 use PHP\DesignPattern\CalculadoraDeImpostos;
-use PHP\DesignPattern\Impostos\Icms;
-use PHP\DesignPattern\Impostos\Iss;
+use PHP\DesignPattern\Impostos\{Icms, Iss};
 use PHP\DesignPattern\Orcamento;
 
 require 'vendor/autoload.php';
 
-$calculadora = new CalculadoraDeImpostos();
+/*$calculadora = new CalculadoraDeImpostos();
 
 $orcamento = new Orcamento();
 $orçamento->valor = 100;
 
 echo $calculadora->calcula($orcamento, new Iss());
+*/
+$calculadora = new CalculadoraDeDescontos();
+
+$orcamento = new Orcamento();
+$orcamento->valor = 200;
+$orcamento->quantidadeItens = 5;
+
+$calculadora->calculaDescontos($orcamento);
